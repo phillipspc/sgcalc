7dd6719de2529efd72371324e12b985959be6c10
Peter Phillips <peter@ib5k.com>
2015-11-25 13:55:30 -0500

    Add 3 rounds to seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  rounds = Round.create([
+    {date_played: Date.today - 1.day, course: 'Broad Bay', notes: 'Played with Dad'},
+    {date_played: Date.today - 2.days, course: "Owl's Creek", notes: 'Nice day'},
+    {date_played: Date.today - 3.days, course: 'Cavalier', notes: 'Good round!'}])
+
+  rounds.each do |round|
+    round.holes.each do |hole|
+      strokes = Stroke.create([
+        {number: 1, hole_id: hole.id, start_distance: rand(300..400), surface: 'Tee'},
+        {number: 2, hole_id: hole.id, start_distance: rand(50..150), surface: 'Fairway'},
+        {number: 3, hole_id: hole.id, start_distance: rand(1..20), surface: 'Green'}])
+    end
+  end
