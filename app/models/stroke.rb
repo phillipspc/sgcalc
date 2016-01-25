d3a71ab4ee4eff9064a08771df6fb22dbefd0854
Peter Phillips <peter@ib5k.com>
2016-01-24 21:23:08 -0500

    Add validation for available data on stroke.

--- app/models/stroke.rb
+++ app/models/stroke.rb
@@ -18,6 +18,7 @@ class Stroke < ActiveRecord::Base
 
   validates_presence_of :start_distance, :surface, :number
   validates_inclusion_of :surface, in: ['Tee', 'Fairway', 'Rough', 'Sand', 'Recovery', 'Green']
+  validate :data_available
 
   def pro_strokes
     return nil unless start_distance && surface
@@ -28,18 +29,10 @@ class Stroke < ActiveRecord::Base
 
   def strokes_gained
     return nil unless start_distance && surface
-    if last?
-      if penalty.present?
-        return pro_strokes - 1 - penalty
-      else
-        return pro_strokes - 1
-      end
-    end
-    if penalty.present?
-      pro_strokes - 1 - next_stroke.pro_strokes - penalty
-    else
-      pro_strokes - 1 - next_stroke.pro_strokes
-    end
+    sg = pro_strokes - 1
+    sg -= 1 if penalty.present?
+    sg -= next_stroke.pro_strokes unless last?
+    return sg
   end
 
   def last?
@@ -49,5 +42,24 @@ class Stroke < ActiveRecord::Base
   def next_stroke
     return nil if last?
     hole.strokes.where("number = ? + 1", number).first
+  end
+
+  def data_available
+    if pro_strokes == 0 || pro_strokes == nil
+      case surface
+      when 'Tee'
+        errors.add(:start_distance) << "must be between 100 and 600 yards for a Tee shot"
+      when 'Fairway'
+        errors.add(:start_distance) << "must be between 10 and 600 yards for a Fairway shot"
+      when 'Rough'
+        errors.add(:start_distance) << "must be between 10 and 600 yards for a shot from Rough"
+      when 'Sand'
+        errors.add(:start_distance) << "must be between 10 and 600 yards for a shot from Sand"
+      when 'Recovery'
+        errors.add(:start_distance) << "must be between 81 and 600 yards for a Recovery shot"
+      when 'Green'
+        errors.add(:start_distance) << "must be between 1 and 115 feet for a putt"
+      end
+    end
   end
 end
