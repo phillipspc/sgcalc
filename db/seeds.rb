72dfca80cac80d40b0b238c3a45f07ea3dfe557d
Peter Phillips <peter@ib5k.com>
2016-04-15 17:26:59 -0400

    make seed rounds more realistic

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,82 @@
 
   rounds.each do |round|
     round.holes.each do |hole|
-      strokes = Stroke.create([
-        {number: 1, hole_id: hole.id, start_distance: rand(300..400), surface: 'Tee'},
-        {number: 2, hole_id: hole.id, start_distance: rand(50..150), surface: 'Fairway'},
-        {number: 3, hole_id: hole.id, start_distance: rand(1..20), surface: 'Green'}])
+      get_par_num = rand(0..2)
+      get_extra_stroke = rand(0..2)
+
+      if get_par_num == 0
+        # Generates strokes for a par 3
+        if get_extra_stroke == 0
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(150..250), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'}
+            ])
+        elsif get_extra_stroke == 1
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(150..250), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'},
+            {number: 3, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'}
+            ])
+        else
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(150..250), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'},
+            {number: 3, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'},
+            {number: 4, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'}
+            ])
+        end
+      elsif get_par_num == 1
+        # Generates strokes for a par 4
+        if get_extra_stroke == 0
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(300..450), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(50..150), surface: 'Fairway'},
+            {number: 3, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'}
+            ])
+        elsif get_extra_stroke == 1
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(300..450), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(50..150), surface: 'Fairway'},
+            {number: 3, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'},
+            {number: 4, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'}
+            ])
+        else
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(300..450), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(50..150), surface: 'Fairway'},
+            {number: 3, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'},
+            {number: 4, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'},
+            {number: 5, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'}
+            ])
+        end
+      elsif get_par_num == 2
+        # Generates strokes for a par 5
+        if get_extra_stroke == 0
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(450..600), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(200..400), surface: 'Fairway'},
+            {number: 3, hole_id: hole.id, start_distance: rand(50..150), surface: 'Fairway'},
+            {number: 4, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'}
+            ])
+        elsif get_extra_stroke == 1
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(450..600), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(200..400), surface: 'Fairway'},
+            {number: 3, hole_id: hole.id, start_distance: rand(50..150), surface: 'Fairway'},
+            {number: 4, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'},
+            {number: 5, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'}
+            ])
+        else
+          Stroke.create([
+            {number: 1, hole_id: hole.id, start_distance: rand(450..600), surface: 'Tee'},
+            {number: 2, hole_id: hole.id, start_distance: rand(200..400), surface: 'Fairway'},
+            {number: 3, hole_id: hole.id, start_distance: rand(50..150), surface: 'Fairway'},
+            {number: 4, hole_id: hole.id, start_distance: rand(5..20), surface: 'Green'},
+            {number: 5, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'},
+            {number: 6, hole_id: hole.id, start_distance: rand(1..4), surface: 'Green'}
+            ])
+        end
+      end
+
     end
   end
